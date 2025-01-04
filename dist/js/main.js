2429196a791060f545f3353e5819fffc0b7aa3c5
Benchuk Dima <dimitron400033@gmail.com>
2025-01-04 20:43:16 +0300

    Создал скрипт getAndSendData, где получил данные из db.json при помощи функции getData и отправил их на сервер через функцию sendData

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,96 @@
-(()=>{"use strict";console.log("getAndSendData")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_getAndSendData__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/getAndSendData */ \"./modules/getAndSendData.js\");\n\r\n\r\n(0,_modules_getAndSendData__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./modules/getAndSendData.js":
+/*!***********************************!*\
+  !*** ./modules/getAndSendData.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst getAndSendData = () => {\r\n  const getData = () => {\r\n    fetch(\"../../db.json\")\r\n      .then((response) => response.json())\r\n      .then((data) => sendDataXMLHttpRequest(data))\r\n      .catch((err) => console.log(err));\r\n  };\r\n\r\n  const sendData = (data) => {\r\n    fetch(\"https://jsonplaceholder.typicode.com/posts\", {\r\n      method: \"POST\",\r\n      body: JSON.stringify(data),\r\n      headers: {\r\n        \"Content-type\": \"application/json; charset=UTF-8\",\r\n      },\r\n    })\r\n      .then((response) => response.json())\r\n      .then((data) => console.log(data))\r\n      .catch((err) => console.log(err));\r\n  };\r\n\r\n  const sendDataXMLHttpRequest = (data) => {\r\n    let xhr = new XMLHttpRequest();\r\n    xhr.open(\"POST\", \"https://jsonplaceholder.typicode.com/posts\");\r\n    xhr.setRequestHeader(\"Content-type\", \"application/json; charset=utf-8\");\r\n    xhr.send(JSON.stringify(data));\r\n\r\n    xhr.onloadend = function () {\r\n      if (xhr.status == 201) {\r\n        console.log(\"Успех\");\r\n      } else {\r\n        console.log(\"Ошибка \" + this.status);\r\n      }\r\n    };\r\n  };\r\n\r\n  getData();\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getAndSendData);\r\n\n\n//# sourceURL=webpack:///./modules/getAndSendData.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./index.js");
+/******/ 	
+/******/ })()
+;
--- src/modules/getAndSendData.js
+++ src/modules/getAndSendData.js
@@ -1,5 +1,40 @@
 const getAndSendData = () => {
-  console.log("getAndSendData");
+  const getData = () => {
+    fetch("../../db.json")
+      .then((response) => response.json())
+      .then((data) => sendDataXMLHttpRequest(data))
+      .catch((err) => console.log(err));
+  };
+
+  const sendData = (data) => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  const sendDataXMLHttpRequest = (data) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhr.send(JSON.stringify(data));
+
+    xhr.onloadend = function () {
+      if (xhr.status == 201) {
+        console.log("Успех");
+      } else {
+        console.log("Ошибка " + this.status);
+      }
+    };
+  };
+
+  getData();
 };
 
 export default getAndSendData;
